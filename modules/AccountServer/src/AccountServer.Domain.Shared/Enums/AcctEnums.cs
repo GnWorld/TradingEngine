505bff99d664e97371171dbbb168208ba9d59deb
广良 王 <wangglnet@outlook.com>
2021-10-21 17:55:27 +0800

    交易账户和多币种子账户表设计

---
+++ modules/AccountServer/src/AccountServer.Domain.Shared/Enums/AcctEnums.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountServer.Enums
+{
+    public enum AcctTypeEnum
+    {
+        /// <summary>
+        /// 普通账户（现金账户，类型：多币种账户,OTC买卖交易账户）
+        /// </summary>
+        Normal = 0,
+        /// <summary>
+        /// 做市账户（类型：单币种账户）
+        /// </summary>
+        Make = 1,
+        /// <summary>
+        /// 撮合账户（类型：多币种账户）
+        /// </summary>
+        Match = 2,
+        /// <summary>
+        /// 配资账户(类型：单币种账户)
+        /// </summary>
+        CapAlloc = 3
+
+
+    }
+
+    public enum AcctRealDemoEnum
+    {
+        /// <summary>
+        /// 真实账户
+        /// </summary>
+        Real = 0,
+        /// <summary>
+        /// 模拟账户
+        /// </summary>
+        Demo = 1
+
+    }
+    public enum AcctStatusEnum
+    {
+        /// <summary>
+        /// 正常
+        /// </summary>
+        Normal = 0,
+        /// <summary>
+        /// 冻结
+        /// </summary>
+        Frozen = 1,
+        /// <summary>
+        /// 冻结且不可查询
+        /// </summary>
+        FrozenInvisible = 2,
+
+        /// <summary>
+        /// 限制交易
+        /// </summary>
+        LimitTrade = 3
+
+    }
+
+
+}
---
+++ modules/AccountServer/src/AccountServer.Domain/Account/TradeAccount.cs
@@ -0,0 +1,69 @@
+﻿using AccountServer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Domain.Entities;
+
+namespace AccountServer.Account
+{
+    /// <summary>
+    /// 账户
+    /// </summary>
+    public class TradeAccount : Entity<Guid>
+    {
+
+
+        /// <summary>
+        /// 拥有人
+        /// </summary>
+        public Guid OwnerId { get; set; }
+
+        /// <summary>
+        /// 拥有人类型 0-普通用户  1-商户 2-平台账户
+        /// </summary>
+        public int OwnerType { get; set; }
+
+
+        /// <summary>
+        /// 余额
+        /// </summary>
+        public decimal Balance { get; set; }
+
+        /// <summary>
+        /// 冻结
+        /// </summary>
+        public decimal Frozen { get; set; }
+
+        /// <summary>
+        /// 币种Code
+        /// </summary>
+        public string CoinCode { get; set; }
+        /// <summary>
+        /// 账户类型
+        /// </summary>
+        public AcctTypeEnum AcctType { get; set; }
+
+        /// <summary>
+        /// 真实/模拟
+        /// </summary>
+        public AcctRealDemoEnum RealDemo { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+
+
+        public int AcctStatus { get; set; }
+
+        /// <summary>
+        /// 支付密码(MD5加密)
+        /// </summary>
+        public string PayPwd { get; set; }
+
+        /// <summary>
+        /// 只读密码(加密)
+        /// </summary>
+        public string ReadOnlyPwd { get; set; }
+    }
+}
